4671e1df1808199406905369dd654b015ef67dd2
Michael Rose Jr <michael.rose@hpe.com>
2022-11-22 11:21:12 -0800

    Updated test data

--- find_rogues/mailer.py
+++ find_rogues/mailer.py
@@ -9,76 +9,6 @@ from jinja2 import (Environment, FileSystemLoader, PackageLoader,
 from loguru import logger
 from sendgrid.helpers.mail import Mail
 
-rdata = [
-    {
-        'acknowledged': False,
-        'classification': 'Rogue',
-        'containment_status': 'Un-contained',
-        'cust_id': '5bf9f40bc876413dac2a533d392d6fa1',
-        'encryption': 'OPEN',
-        'first_det_device': 'CNH2HN797D',
-        'first_det_device_name': 's1262ap3',
-        'first_seen': '2022-04-07T06:41:02.515Z',
-        'group_name': 'West -1',
-        'id': '00:30:44:19:C5:40',
-        'labels': 'Some(505)',
-        'last_det_device': 'CNH2HN7972',
-        'last_det_device_name': 's1262ap1',
-        'last_seen': '2022-11-21T17:04:24.244Z',
-        'mac_vendor': 'CradlePoint, Inc',
-        'name': 'CradlePoint,-19:C5:40',
-        'overriden': False,
-        'signal': -21,
-        'ssid': 'SephoraFreeWifi',
-        'human_first_seen': 'Thursday, 07-Apr-2022 06:41:02 UTC'
-    },
-    {
-        'acknowledged': False,
-        'classification': 'Rogue',
-        'containment_status': 'Un-contained',
-        'cust_id': '5bf9f40bc876413dac2a533d392d6fa1',
-        'encryption': 'WPA',
-        'first_det_device': 'PHL4KD56BD',
-        'first_det_device_name': 's212ap1',
-        'first_seen': '2021-12-11T02:32:00.101Z',
-        'group_name': 'East -2',
-        'id': '00:30:44:2B:6B:40',
-        'labels': 'Some(180)',
-        'last_det_device': 'PHL4KD56BG',
-        'last_det_device_name': 's212ap2',
-        'last_seen': '2022-11-21T17:57:13.800Z',
-        'mac_vendor': 'CradlePoint, Inc',
-        'name': 'CradlePoint,-2B:6B:40',
-        'overriden': False,
-        'signal': -24,
-        'ssid': 'teamwork',
-        'human_first_seen': 'Saturday, 11-Dec-2021 02:32:00 UTC'
-    },
-    {
-        'acknowledged': False,
-        'classification': 'Rogue',
-        'containment_status': 'Un-contained',
-        'cust_id': '5bf9f40bc876413dac2a533d392d6fa1',
-        'encryption': 'OPEN',
-        'first_det_device': 'PHL4KD56BD',
-        'first_det_device_name': 's212ap1',
-        'first_seen': '2022-04-01T18:37:50.487Z',
-        'group_name': 'East -2',
-        'id': '02:30:44:2B:6B:40',
-        'labels': 'Some(180)',
-        'last_det_device': 'PHL4KD56BG',
-        'last_det_device_name': 's212ap2',
-        'last_seen': '2022-11-21T17:57:13.749Z',
-        'mac_vendor': '02:30:44:2B:6B:40',
-        'name': '02:30:44:2B:6B:40',
-        'overriden': False,
-        'signal': -19,
-        'ssid': 'Sephora Free WiFi',
-        'human_first_seen': 'Friday, 01-Apr-2022 18:37:50 UTC'
-    }
-]
-
-
 
 def sendmail(message_details, config, html_template_content):
     message = Mail(
@@ -126,5 +56,6 @@ if __name__ == "__main__":
     # sendmail("test")
     print(account['sendgrid_api_key'])
     print(message_details)
+    rdata = []
     # sendmail(message_details, account)
     send_template_email('rogues_found.html.jinja2', message_details, rdata, rdata, account=account)
